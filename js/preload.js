00ff83745f280f2f2027fb3093b143874bdf1e9e
k-praaew <i_love_prew@hotmail.com>
2019-03-22 23:07:00 +0700

    aaa

---
+++ js/preload.js
@@ -0,0 +1,18 @@
+(function() {
+
+	var preload = document.getElementById("loader");
+	var loading = 0;
+	var id = setInterval(frame, 64);
+	
+	function frame(){
+		if(loading == 10){
+			clearInterval(id);
+			window.open("home.html","_self");
+		}else{
+			loading = loading + 1;
+			if(loading == 5){
+				preload.style.animation = "fadeout is ease";
+			}
+		}
+	}
+})();
